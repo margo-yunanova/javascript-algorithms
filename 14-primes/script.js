2e58d0d34688b6a1a6f04081fbd26dcb99b054cb
Margo Yunanova <m.yunanova@gmail.com>
2022-08-28 19:18:53 +0300

    add primes solution; close #10

--- 14-primes/script.js
+++ 14-primes/script.js
@@ -3,11 +3,39 @@
  *
  * Напишите функцию primes(n). Её единственный аргумент — целое число n.
  * Функция должна возвращать массив простых чисел от 2 до n.
- * 
+ *
 */
 
+/**
+ * @param {number} number
+ * @returns {Boolean}
+ */
+
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
+  } else {
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+/**
+ * @param {number} num
+ * @returns {number[]}
+ */
 function primes(num) {
-    // Напишите код здесь
+  let arrayPrimes = [];
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      arrayPrimes.push(i);
+    }
+  }
+  return arrayPrimes;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
