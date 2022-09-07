713012955ddc29a11c082ba0d682c8b02b6c9b44
Margo Yunanova <m.yunanova@gmail.com>
2022-09-07 14:54:14 +0300

    add capitalize solution; close #5

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 /*
  * Задача 4: «С большой буквы»
  *
@@ -6,10 +8,38 @@
  *
 */
 
-function capitalize(str) {
-    // Напишите код здесь
+/**
+ *
+ * @param {string} str
+ * @returns {string} capitalStr
+ */
+
+function capitalize2(str) {
+  let capitalStr = '';
+  let char;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      char = i
+    }
+    capitalStr = capitalStr + str[i]
+  }
+  return capitalStr;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(capitalize('молодость всё простит')); // "Молодость Всё Простит"
+console.log(capitalize2('молодость всё простит'));
+
+/**
+ *
+ * @param {string} str
+ * @returns {string} capitalStr
+ */
+
+function capitalize(str) {
+  return str.split(' ').map(item => item === '' ? '' : item[0].toUpperCase() + item.slice(1)).join(' ');
+}
+
+// Протестируйте решение, вызывая функцию с разными аргументами:
+
+console.log(capitalize('молодость всё простит'));
