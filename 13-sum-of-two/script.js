6cafb8334bc30952d0ef271794709b40653ab385
Margo <m.yunanova@gmail.com>
2022-11-08 19:04:06 +0300

    implement sum-of-two solution

--- 13-sum-of-two/script.js
+++ 13-sum-of-two/script.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 /*
  * Задача 13: «Сумма двух»
  *
@@ -5,14 +7,27 @@
  * и целое число num. Функция должна вернуть true, если в переданном массиве
  * есть какие-то два числа, чья сумма равна num. Если же такой пары чисел нет,
  * функция должна вернуть false.
- * 
+ *
 */
+/**
+ * @param {number[]} arr
+ * @param {number} sum
+ * @returns {boolean}
+ */
 
-function sumOfTwo(arr, sum) {
-    // Напишите код здесь
+ function sumOfTwo(arr, sum) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1 + i; j < arr.length; j++) {
+      if (arr[i] + arr[j] === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(sumOfTwo([1, 2, 3, 4, 5], 4)); // true (так как 1 + 3 === 4)
-console.log(sumOfTwo([1, 2, 3, 4, 5], 100)); // false
+console.log(sumOfTwo([1, 2, 3, 4, 5], 100))
+console.log(sumOfTwo([1, -1, 1], 0)); // false
