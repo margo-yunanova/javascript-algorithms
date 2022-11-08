2bdc691bf3f673756cb9d9e46ddb09414f8469b7
Margo <m.yunanova@gmail.com>
2022-11-08 19:00:27 +0300

    Revert "implement sumOfTwo solution"
    This reverts commit d629c594c71bbd3c8bc065c4a2fe544f0d361662.

--- 13-sum-of-two/script.js
+++ 13-sum-of-two/script.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 /*
  * Задача 13: «Сумма двух»
  *
@@ -7,27 +5,14 @@
  * и целое число num. Функция должна вернуть true, если в переданном массиве
  * есть какие-то два числа, чья сумма равна num. Если же такой пары чисел нет,
  * функция должна вернуть false.
- *
+ * 
 */
-/**
- * @param {number[]} arr
- * @param {number} sum
- * @returns {boolean}
- */
 
 function sumOfTwo(arr, sum) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1 + i; j < arr.length; j++) {
-      if (arr[i] + arr[j] === sum) {
-        return true;
-      }
-    }
-  }
-  return false;
+    // Напишите код здесь
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(sumOfTwo([1, 2, 3, 4, 5], 4)); // true (так как 1 + 3 === 4)
-console.log(sumOfTwo([1, 2, 3, 4, 5], 100))
-console.log(sumOfTwo([1, -1, 1], 0)); // false
+console.log(sumOfTwo([1, 2, 3, 4, 5], 100)); // false
