3a132004ee80f6e62e6e9dd9ebc4e802a1475536
Margo <m.yunanova@gmail.com>
2022-11-08 21:11:36 +0300

    implement anagram solution

--- 07-anagram/script.js
+++ 07-anagram/script.js
@@ -1,3 +1,6 @@
+// @ts-check
+
+
 /*
  * Задача 7: «Анаграмма»
  *
@@ -5,12 +8,33 @@
  * Напишите функцию, проверяющую, являются ли две строки анаграммами друг друга
  * (регистр букв не имеет значения). Для простоты примите, что в этих строках
  * нет пробелов и знаков препинания.
- * 
+ *
 */
 
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @returns {boolean}
+ */
+
 function anagram(str1, str2) {
-    // Напишите код здесь
+  if (str1.toLowerCase() === str2.toLowerCase() || str1.length !== str2.length) {
+    return false;
+  }
+  let arr1 = str1.toLowerCase().split('').sort();
+  let arr2 = str2.toLowerCase().split('').sort();
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      continue;
+    }
+  }
+  return true;
 }
+
+
+
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
