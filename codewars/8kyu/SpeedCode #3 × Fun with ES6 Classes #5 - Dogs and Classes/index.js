d7e7c1a06d3897f17787b4a415157aa792dd0946
Margo <m.yunanova@gmail.com>
2022-11-26 15:22:12 +0300

    solution SpeedCode 3 × Fun with ES6 Classes 5 - Dogs and Classes #23

---
+++ codewars/8kyu/SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes/index.js
@@ -0,0 +1,22 @@
+class Dog {
+  constructor(name, age, gender, species, size, master, loyal) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.species = species;
+    this.legs = 4;
+    this.size = size;
+    this.master = master;
+    this.loyal = loyal;
+  }
+}
+
+class Labrador extends Dog {
+  constructor(name, age, gender, master) {
+    super(name, age, gender, master)
+    this.species = "Labrador";
+    this.size = "Large";
+    this.loyal = true;
+    this.master = master;
+  }
+}
