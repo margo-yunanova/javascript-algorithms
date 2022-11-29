3e5c451083d78ed510ebb4e58469eafa75916558
Margo <m.yunanova@gmail.com>
2022-11-29 14:34:40 +0300

    refactoring Format words into a sentence close #27

--- codewars/6kyu/Format words into a sentence/index.js
+++ codewars/6kyu/Format words into a sentence/index.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-const isSpace = (symbol) => symbol === '';
+// const isEmpty = (string) => string === '';
 
 /**
  *
@@ -11,13 +11,13 @@ const isSpace = (symbol) => symbol === '';
 function formatWords(words) {
   if (words === null || !words.join()) return '';
 
-  const arr = words.filter(word => !isSpace(word));
+  const arr = words.filter(word => word !== '');
 
-  return arr.length === 1 ? arr.join() : `${arr.slice(0, arr.length - 1).join(', ')} and ${arr[arr.length - 1]}`;
+  return arr.length === 1 ? arr.join() : `${arr.slice(0, -1).join(', ')} and ${arr[arr.length - 1]}`;
 }
 
 console.log(formatWords(['one', 'two', 'three', 'four']));
-console.log(formatWords(['', '', 'three']));
-console.log(formatWords([]));
-console.log(formatWords(null));
-console.log(formatWords(['']));
+// console.log(formatWords(['', '', 'three']));
+// console.log(formatWords([]));
+// console.log(formatWords(null));
+// console.log(formatWords(['']));
