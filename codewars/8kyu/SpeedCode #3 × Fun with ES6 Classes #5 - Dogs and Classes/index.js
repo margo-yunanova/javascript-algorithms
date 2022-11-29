d6447ef360d558ce9a2ab108f134196f7c1bf886
Margo <m.yunanova@gmail.com>
2022-11-29 14:35:56 +0300

    refactoring Dogs and Classes close #23

--- codewars/8kyu/SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes/index.js
+++ codewars/8kyu/SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes/index.js
@@ -13,10 +13,6 @@ class Dog {
 
 class Labrador extends Dog {
   constructor(name, age, gender, master) {
-    super(name, age, gender, master)
-    this.species = "Labrador";
-    this.size = "Large";
-    this.loyal = true;
-    this.master = master;
+    super(name, age, gender, "Labrador", "Large", master, true);
   }
 }
