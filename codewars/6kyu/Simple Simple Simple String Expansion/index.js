870c539f0d0c3169a5db5e7d89337ffd79567010
Margo <m.yunanova@gmail.com>
2023-02-21 15:25:03 +0300

    solution for string expansion using map

--- codewars/6kyu/Simple Simple Simple String Expansion/index.js
+++ codewars/6kyu/Simple Simple Simple String Expansion/index.js
@@ -2,13 +2,27 @@ const numbers = "0123456789";
 
 const stringExpansion = (string) => {
   let number = 1;
-  return string.split("").reduce((newStr, char) => {
+  return string.split("").map((char) => {
     if (numbers.includes(char)) {
-      number = char;
-      return newStr;
+      number = +char;
+      return "";
     }
-    return newStr + char.repeat(number);
-  }, "");
+    return char.repeat(number);
+  }).join("");
 };
 
 console.log(stringExpansion("3d332f2a"));
+
+
+const stringExpansion2 = (string) => {
+  let number = 1;
+  return string.split("").flatMap((char) => {
+    if (numbers.includes(char)) {
+      number = +char;
+      return [];
+    }
+    return Array(number).fill(char);
+  }).join("");
+};
+
+console.log(stringExpansion2("3d332f2a"));
