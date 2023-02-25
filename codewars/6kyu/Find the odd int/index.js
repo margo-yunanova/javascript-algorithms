fe1a901a5a7f9edc22b17712ec786b019171431c
Margo <m.yunanova@gmail.com>
2023-02-25 12:13:00 +0300

    second solution for findOdd

--- codewars/6kyu/Find the odd int/index.js
+++ codewars/6kyu/Find the odd int/index.js
@@ -1,22 +1,26 @@
-const findOdd = (array) => {
-  if (array.length === 1) return array[0];
+const findOdd = (array) => array.find(element => (array.reduce((sum, item) => element === item ? sum + 1 : sum, 0) % 2) !== 0);
 
-  const checkedNumbers = [];
+//const findOdd = (array) => array.find(element => array.filter(item => element === item).length % 2 !== 0);
 
-  for (let i = 0; i < array.length; i++) {
-    if (!checkedNumbers.includes(array[i])) {
-      checkedNumbers.push(array[i]);
-      let count = 1;
+console.log(findOdd([1,1,1,1,1,1,10,1,1,1,1]));
 
-      for (let j = i + 1; j < array.length; j++) {
-        if (array[i] === array[j]) {
-          count++
-        };
-      }
+// const findOdd = (array) => {
+//   if (array.length === 1) return array[0];
 
-      if (count % 2 !== 0) return array[i];
-    }
-  }
-};
+//   const checkedNumbers = [];
 
-console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
+//   for (let i = 0; i < array.length; i++) {
+//     if (!checkedNumbers.includes(array[i])) {
+//       checkedNumbers.push(array[i]);
+//       let count = 1;
+
+//       for (let j = i + 1; j < array.length; j++) {
+//         if (array[i] === array[j]) {
+//           count++
+//         };
+//       }
+
+//       if (count % 2 !== 0) return array[i];
+//     }
+//   }
+// };
