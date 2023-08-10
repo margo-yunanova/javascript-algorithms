c04addf80a53cec64c00234a114269b577bb4be0
Margo <m.yunanova@gmail.com>
2023-08-10 11:27:56 +0300

    solution for twoSum

---
+++ leetcode/Two Sum/index.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0, j = i + 1; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 2, 3], 6));
